9d3d82d046b4e682632ee7c19a5eff55d02aedcb
digitalRepresentation <kinemura@naver.com>
2020-11-03 23:22:34 +0900

    Initial Commit

---
+++ swiftStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftStudy
+//
+//  Created by 金宝ヨン on 2020/11/03.
+//  Copyright © 2020 kimjinyoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
