70b2c75fd6e6316dcdde4be18e0ba858e38dcf2c
digitalRepresentation <kinemura@naver.com>
2020-11-11 19:49:25 +0900

    basetype

---
+++ Yagom/05-01basedatatype.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+// Swiftの基本タイプ
+// Bool, Int, UInt, Float, Double, Character, String
+
+// Bool
+var someBool: Bool = true
+someBool = false
+// someBool = 0
+// someBool = 1
+
+// Int
+var someInt: Int = -100
+//someInt = 100.1
+
+// UInt
+var someUInt: UInt = 100
+//someUInt = -100
+//someUInt = someInt
+
+// Float
+var someFloat: Float = 3.14
+someFloat = 3
+
+// Double
+var someDouble: Double = 3.14
+someDouble = 3
+//someDouble = someFloat
+
+//Charcter
+var someCharacter: Character = "🇯🇵"
+//someCharacter = "☺️"
+//someCharacter = "あ"
+//someCharacter = "A"
+//someCharacter = "笑"
+//someCharacter = "ははは"
+print(someCharacter)
+
+//String
+var someString: String = "ははは　☺️　"
+someString = someString + "笑ったら福がきますよ"
+print(someString)
+
+//someString = someCharacter
+
