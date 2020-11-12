0f622687a5d950cd0d7a02eca76371bd0006f37d
digitalRepresentation <kinemura@naver.com>
2020-11-12 23:31:35 +0900

    Optional 説明

---
+++ Yagom/07-01Optional.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//MARK - Optional
+//値があるかもないかも
+
+let optionalConstant: Int? = nil
+
+//let someConstant: Int = nil
+
+//nilの可能性を明示に表現
+//nil可能性をドキュメント化しなくてもコードだけで表現ができる
+//ドキュメント／コメント作成時間節約
+//伝達もらった値がOptionalではなければnilチェックをしなくても安心して使用
+//効率的なコーディング
+//例外状況を再消化する安全なコーディング
