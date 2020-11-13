873b629c57afd79b9c936b24b0f93b515de5f70b
digitalRepresentation <kinemura@naver.com>
2020-11-13 23:20:55 +0900

    optional 1/2

--- Yagom/07-01Optional.playground/Contents.swift
+++ Yagom/07-01Optional.playground/Contents.swift
@@ -12,4 +12,40 @@ let optionalConstant: Int? = nil
 //ドキュメント／コメント作成時間節約
 //伝達もらった値がOptionalではなければnilチェックをしなくても安心して使用
 //効率的なコーディング
-//例外状況を再消化する安全なコーディング
+//例外状況を再消化する安全なコーディン
+
+// someOptionalParam can be nil
+func someFunction(someOptionalParam: Int?) {
+    //...
+}
+
+// someParam must not be nil
+func someFunction(someParam: Int) {
+    //...
+}
+
+someFunction(someOptionalParam: nil)
+//someFunction(someParam: nil)
+
+enum Optional<Wrapped> : ExpressibleByNilLiteral {
+    init(nilLiteral: ()) {
+        <#code#>
+    }
+    
+    case none
+    case some(Wrapped)
+}
+
+let optionalValue: Optional<Int> = nil
+let optionalValue2: Int? = nil
+
+//liplicitly Unwrapped Optional
+//暗黙的に抽出オプショナル
+
+//var optionalValue: Int! = 100
+//
+//switch optionalValue {
+//case .none:
+//    print("This Optional variable is nil")
+//
+//}
