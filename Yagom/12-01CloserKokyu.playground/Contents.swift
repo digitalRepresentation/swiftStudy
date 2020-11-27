00385c8275b736393fbe2257cb82d964460e678f
digitalRepresentation <kinemura@naver.com>
2020-11-28 00:52:47 +0900

    Closer 高級 1/3

--- Yagom/12-01CloserKokyu.playground/Contents.swift
+++ Yagom/12-01CloserKokyu.playground/Contents.swift
@@ -2,6 +2,6 @@ import UIKit
 
 /* Closer 高級*/
 
-func calculate(a: Int, b: Int, method: (Int, Int) -> Int {
-    return method(a, b)
-}
+//func calculate(a: Int, b: Int, method: (Int, Int) -> Int {
+//    return method(a, b)
+//})
