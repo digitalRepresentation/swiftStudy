7443ac7446f848c6b17a1ffd596d6beff38eeaf5
digitalRepresentation <kinemura@naver.com>
2020-12-01 23:40:47 +0900

    高級Closer

--- Yagom/12-01CloserKokyu.playground/Contents.swift
+++ Yagom/12-01CloserKokyu.playground/Contents.swift
@@ -1,7 +1,24 @@
 import UIKit
 
-/* Closer 高級*/
+/*
+　Trailing Closer
+ returnタイプ
+ 
+ 
+ */
 
-//func calculate(a: Int, b: Int, method: (Int, Int) -> Int {
-//    return method(a, b)
-//})
+
+func calculate(a: Int, b: Int, method: (Int, Int) -> Int) -> Int {
+    return method(a, b)
+}
+
+var result: Int
+
+//MARK: - Trailing Closer
+//Closerが関数の最後のパラメーターなら
+//最後のパラメーター名前を総略した後
+//関数の小括弧の外部にCloserを実装できる
+
+result = calculate(a: 10, b: 10) { (left: Int, right: Int) -> Int in
+    return left + right
+}
