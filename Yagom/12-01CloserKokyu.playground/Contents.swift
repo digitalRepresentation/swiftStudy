cd27cd4b6358f4abea7525d645ab541038ae3b9b
digitalRepresentation <kinemura@naver.com>
2020-12-06 23:17:53 +0900

    1/3 クローザー高級

--- Yagom/12-01CloserKokyu.playground/Contents.swift
+++ Yagom/12-01CloserKokyu.playground/Contents.swift
@@ -19,6 +19,22 @@ var result: Int
 //最後のパラメーター名前を総略した後
 //関数の小括弧の外部にCloserを実装できる
 
-result = calculate(a: 40, b: 10) { (left: Int, right: Int) -> Int in
+result = calculate(a: 10, b: 10) { (left: Int, right: Int) -> Int in
     return left + right
 }
+
+//MARK: - 変換タイプ省略
+// calculate関数のmethodパラメーターは
+// Intタイプを変換タイプする事実をコンパイラーも分かるため
+// あえてクローザーで変換タイプを明示しなくても大丈夫です。
+
+print(result)
+
+// Trailing Closerと一緒に使用ができます。
+result = calculate(a:10, b:10) { (left: Int, right: Int) in
+    return left + right
+}
+
+
+
+
